8c5e77dc329506e100e09af2e87c64773476e07e
thiagoluizoliveira <42360276+thiagoluizoliveira@users.noreply.github.com>
2018-09-22 23:30:49 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,3 +1,26 @@
+language: python
+python:
+ - "3.4"
+script:
+ - cd src; pyhton testes.py
+
+def inicializar():
+	tab - []
+	for i in range(3):
+		linha = []
+		for j in range(3):
+			linha.append(".")
+		tab.append(linha)
+	return tab
+
+def main ():
+	jogo = inicializar()
+	print(jogo)
+if __name__ == "__main__":
+	main()
+
+testes.py
+
 import jogovelha
 import sys
 
@@ -9,7 +32,7 @@ if len (jogo) != 3:
 	erroInicializar = True
 else:
 	for linha in jogo:
-		if len(linha) "= 3:
+		if len(linha) != 3:
 			erroInicializar = True
 		else:
 			for elemento in linha:
